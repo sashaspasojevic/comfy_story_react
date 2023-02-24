81193f38e72120dc6c3e523e1ee2d5fbea9219e0
sashaspasojevic <spsjvcsasa@yahoo.com>
2023-02-24 14:33:34 +0100

    add stars to single product

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,70 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from "react";
+import styled from "styled-components";
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
+const Stars = ({ stars, reviews }) => {
+  return (
+    <Wrapper>
+      <div className='stars'>
+        {/* star */}
+        <span>
+          {stars >= 1 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 0.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar></BsStar>
+          )}
+        </span>
+        {/* end of star */}
+        {/* star */}
+        <span>
+          {stars >= 2 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 1.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar></BsStar>
+          )}
+        </span>
+        {/* end of star */}
+        {/* star */}
+        <span>
+          {stars >= 3 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 2.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar></BsStar>
+          )}
+        </span>
+        {/* end of star */}
+        {/* star */}
+        <span>
+          {stars >= 4 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 3.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar></BsStar>
+          )}
+        </span>
+        {/* end of star */}
+        {/* star */}
+        <span>
+          {stars === 5 ? (
+            <BsStarFill></BsStarFill>
+          ) : stars >= 4.5 ? (
+            <BsStarHalf></BsStarHalf>
+          ) : (
+            <BsStar></BsStar>
+          )}
+        </span>
+        {/* end of star */}
+      </div>
+      <p className='reviews'>({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +79,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
