48db961fbd5559dd5500ad6c7d2e08a8cc46714d
sashaspasojevic <spsjvcsasa@yahoo.com>
2023-03-07 18:43:16 +0100

    cart funcionality finished

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -17,7 +17,7 @@ const CartContent = () => {
       })}
       <hr />
       <div className='link-container'>
-        <Link to='/product' className='link-btn'>
+        <Link to='/products' className='link-btn'>
           continue shopping
         </Link>
         <button
